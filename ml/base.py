05240a46072868497a157b601832ca7a04ac3633
Moritz Mistol <moritz.mistol@gmail.com>
2021-09-01 13:06:09 +0200

    implement Linear Model base class and regressor mixin class
    These classes are as of yet abstract classes, that only define
    the necessary functions that every subclass has to implement

--- ml/base.py
+++ ml/base.py
@@ -1,7 +1,40 @@
-from abc import ABCMeta
+from __future__ import annotations
+from abc import ABCMeta, abstractmethod
+
+from numpy.typing import ArrayLike
+import numpy as np
 
 
-class BaseEstimator(metaclass=ABCMeta):
-    """Base class for all estimators"""
-    def __init__(self):
-        pass
+class LinearModel(metaclass=ABCMeta):
+    """Base class for all linear models"""
+
+    @abstractmethod
+    def fit(self, X: ArrayLike, y: ArrayLike) -> LinearModel:
+        """Fit model."""
+
+    @abstractmethod
+    def predict(self, X: ArrayLike):
+        """
+        Predict using the linear model.
+
+        :param X: samples
+
+        :type X: array-like, shape (n_samples, n_features)
+
+        :return: the predicted values
+        :rtype: array, shape (n_samples,)
+        """
+
+
+class RegressorMixin(metaclass=ABCMeta):
+    """Mixin class for all regression estimators."""
+
+    _estimator_type = "regressor"
+
+    @abstractmethod
+    def score(self) -> float:
+        """
+        Returns the :math:`R^2` score of the model.
+
+        :return: the score of the model
+        """
