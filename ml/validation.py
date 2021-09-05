b12c839c3eb414dd139038c43b2627feb54e91e2
Moritz Mistol <moritz.mistol@gmail.com>
2021-09-05 18:53:58 +0200

    add check_2D function

--- ml/validation.py
+++ ml/validation.py
@@ -1,6 +1,7 @@
 import numpy as np
 from numpy import ndarray
 
+
 def check_X_y(X, y) -> (ndarray, ndarray):
     """
     Checks for valid X and y inputs.
@@ -22,12 +23,41 @@ def check_X_y(X, y) -> (ndarray, ndarray):
         or y is not 1D
         or if the number of rows in X is not equal to the length of y
     """
-    X = np.array(X)
+    X = check_2D(X, "X")
     y = np.array(y)
-    if X.ndim != 2:
-        raise ValueError(f"X must be 2D, instead got an {X.ndim}D array")
     if y.ndim != 1:
         raise ValueError(f"y must be 1D, instead got an {y.ndim}D array")
     if X.shape[0] != y.shape[0]:
-        raise ValueError("mismatched dimensions of X and y")
+        raise ValueError(f"mismatched dimensions of X and y:"
+                         f" Rows in X = {X.shape[0]} != {y.shape[0]} = length of y")
     return X, y
+
+
+def check_2D(array, arr_name: str = "") -> ndarray:
+    """
+    Checks if the given array is 2D and throws a ValueError otherwise.
+
+    Parameters
+    ----------
+    array : array_like
+        The array to check.
+    arr_name : str, optional
+        The name of the array to display in the error message.
+
+    Returns
+    -------
+    ndarray
+        The checked array.
+
+    Raises
+    ------
+    ValueError
+        If `array` is not 2D.
+    """
+    arr = np.array(array)
+    if arr.ndim != 2:
+        if arr_name:
+            arr_name = " `" + arr_name + "`"
+        msg = f"The input array{arr_name} must be 2D, instead got an {array.ndim}D array."
+        raise ValueError(msg)
+    return arr
