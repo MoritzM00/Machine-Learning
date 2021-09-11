7513e15dca9043a13c7c0c2647ae1420f4bdd540
Moritz Mistol <moritz.mistol@gmail.com>
2021-09-11 11:13:20 +0200

    add docstrings and change computation of pca
    Previously, it was computed using the SVD solver of numpy. Now
    it is computed by first calculating the covariance matrix of X
    and then the eigendecomposition of the cov matrix.

--- ml/decomposition/pca.py
+++ ml/decomposition/pca.py
@@ -1,53 +1,121 @@
 import numpy as np
-from ..validation import check_array
+import numpy.linalg as la
+from ml.validation import check_array
 
 
 class PCA:
-    # TODO: documentation
-    # TODO: testing
+    """
+    Principal Component Analysis.
+
+    Attributes
+    ----------
+    n_components : int
+        Number of principal components.
+    n_samples : int
+        Number of Samples.
+    n_features : int
+        Number of features.
+    components : ndarray of shape (n_features, n_components)
+        Principal Components.
+    eigenvalues : ndarray of shape (n_components,)
+        The eigenvalues of the covariance matrix of the training data.
+    mean : float64
+        Mean of the samples.
+    """
 
     def __init__(self, n_components):
         self.n_components = n_components
         self.n_samples = None
         self.n_features = None
         self.components = None
+        self.eigenvalues = None
         self.mean = None
 
-    def fit(self, X, y=None):
-        self._fit(X)
-        return self
-
     def transform(self, X):
+        """
+        Transforms X using the principal components.
+
+        Parameters
+        ----------
+        X : array_like, shape (n_samples, n_features)
+            The samples.
+
+        Returns
+        -------
+        ndarray of shape (n_samples, n_components)
+            The transformed input matrix `X`
+        """
         X = check_array(X)
         if self.mean is not None:
             X -= self.mean
-        return np.dot(X, self.components.T)
+        return np.dot(X, self.components)
 
     def inverse_transform(self, X):
-        return np.dot(X, self.components) + self.mean
+        """
+        Undoes the transformation to `X`.
+
+        Parameters
+        ----------
+        X : array_like, shape (n_samples, n_features)
+
+        Returns
+        -------
+        ndarray of shape  (n_samples, n_features)
+            The original training data.
+        """
+        return np.dot(X, self.components.T) + self.mean
 
     def fit_transform(self, X, y=None):
-        U, S, Vt = self._fit(X)
-        U = U[:, :self.n_components]
+        """
+        Fits the model to X and transforms X.
+        Equivalent to fit(X).transform(X)
 
-        # X_new = X * V = U * S * V * Vt = U * S
-        # because V (and U) are orthogonal matrices, i.e. V times V transpose is
-        # equal to the (n_features, n_features) identity matrix
-        U *= S[: self.n_components]
-        return U
+        Parameters
+        ----------
+        X : array_like, shape (n_samples, n_features)
+            Training data.
+        y : Ignored
+            No usage. (only for API consistency of fit method)
 
-    def _fit(self, X):
+        Returns
+        -------
+        ndarray of shape (n_samples, n_components)
+            The transformed training data.
+        """
+        return self.fit(X).transform(X)
+
+    def fit(self, X, y=None):
+        """
+        Fits the model using X.
+
+        Parameters
+        ----------
+        X : array_like, shape (n_samples, n_features)
+            Training data.
+        y : Ignored
+            No usage. (only for API consistency of fit method)
+
+        Returns
+        -------
+        self : PCA
+            The fitted model.
+        """
         X = check_array(X)
         self.n_samples, self.n_features = X.shape
-
+        print(X.shape)
         # Center X
         self.mean = X.mean(axis=0)
         X -= self.mean
 
-        # Singular Value Decomposition
-        # U contains the left singular vectors, which are the eigenvectors of X(X.T)
-        # Vt contains the right singular vectors, which are the eigenvectors of X.T X
-        U, S, Vt = np.linalg.svd(X, full_matrices=False)
-        self.components = Vt[, :self.n_components]
+        # get the covariance matrix of the data
+        cov = np.cov(X, rowvar=False)
+        print(cov)
+        # eigendecomposition of the cov matrix
+        eigenvalues, eigenvectors = la.eigh(cov)
+        print(eigenvectors.shape)
 
-        return U, S, Vt
+        # sort the eigenvectors by eigenvalues from largest to smallest
+        idx = eigenvalues.argsort()[::-1][:self.n_components]
+        self.eigenvalues = eigenvalues[idx]
+        self.components = eigenvectors[:, idx]
+        return self
