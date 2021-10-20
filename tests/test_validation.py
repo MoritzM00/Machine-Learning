d08aea54274670401d71d5803dc806ab8c06effe
Moritz Mistol <moritz.mistol@gmail.com>
2021-10-20 18:19:36 +0200

    change test suite to pytest from unittest

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -1,42 +1,25 @@
-import unittest
+import pytest
 import numpy as np
 
 from ml.validation import check_X_y, check_array, check_consistent_length
 
 
-class TestValidation(unittest.TestCase):
-    def test_not_2D(self):
-        X = np.array([1, 2])
-        self.assertRaises(
-            ValueError,
-            lambda: check_array(X)
-        )
-
-    def test_check_X_y(self):
-        X = np.array(
-            [[1, 2, 3],
-             [1, 2, 3]]
-        )
-        y1 = np.array([[1]])
-        y2 = np.array([1])
-        self.assertRaises(
-            ValueError,
-            lambda: check_X_y(X, y1)
-        )
-        self.assertRaises(
-            ValueError,
-            lambda: check_X_y(X, y2)
-        )
-
-    def test_check_consistent_length(self):
-        self.assertRaises(
-            ValueError,
-            lambda: check_consistent_length(
-                [[1, 2]],
-                [1, 2],
-            )
-        )
+def test_not_2D():
+    X = np.array([1, 2])
+    with pytest.raises(ValueError):
+        check_array(X)
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_check_X_y():
+    X = np.array([[1, 2, 3], [1, 2, 3]])
+    y1 = np.array([[1]])
+    y2 = np.array([1])
+    with pytest.raises(ValueError):
+        check_X_y(X, y1)
+    with pytest.raises(ValueError):
+        check_X_y(X, y2)
+
+
+def test_check_consistent_length():
+    with pytest.raises(ValueError):
+        check_consistent_length([[1, 2]], [1, 2])
