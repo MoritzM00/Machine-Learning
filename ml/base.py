20150724cb9d57c14eb04b2cbf1bc48aa7d013a6
Moritz Mistol <moritz.mistol@gmail.com>
2021-10-22 18:00:02 +0200

    update base.py LinearModel now implements the BaseEstimator class of Sklearn to provide compatibility to the sci-kit learn API

--- ml/base.py
+++ ml/base.py
@@ -1,8 +1,11 @@
 from __future__ import annotations
 from abc import ABCMeta, abstractmethod
 
+from sklearn.base import BaseEstimator
+from sklearn.utils.validation import check_is_fitted, check_array
 
-class LinearModel(metaclass=ABCMeta):
+
+class LinearModel(BaseEstimator, metaclass=ABCMeta):
     """Base class for all linear models"""
 
     @abstractmethod
@@ -22,7 +25,6 @@ class LinearModel(metaclass=ABCMeta):
             The fitted model.
         """
 
-    @abstractmethod
     def predict(self, X) -> float:
         """
         Predict using the linear model.
@@ -35,22 +37,39 @@ class LinearModel(metaclass=ABCMeta):
         Returns
         -------
         float
-            The predicted value
+            The predicted value.
         """
+        check_is_fitted(self)
+        X = check_array(X)
+        return X.dot(self.coef_) + self.intercept_
 
 
-class RegressorMixin(metaclass=ABCMeta):
-    """Mixin class for all linear_model estimators."""
+class RegressorMixin:
+    """
+    Mixin for all regressors.
 
-    _estimator_type = "regressor"
+    Implements a score method, which returns the :math:R^2 score of the model.
+    """
 
-    @abstractmethod
-    def score(self) -> float:
+    _estimtor_type = "regressor"
+
+    def score(self, X, y):
         """
-        Calculates the :math:`R^2` score of the model.
+        Returns the score, i.e. the :math:R^2 measure of the model.
+
+        Parameters
+        ----------
+        X : ndarray of shape (n_samples, n_features)
+            The test data.
+        y : ndarray of shape (n_features,)
+            The true values of y for the test data.
 
         Returns
         -------
         float
-            the score of the model
+            The ..math:R^2 score of the model.
         """
+        from ml.metrics.regression import r2_score
+
+        y_pred = self.predict(X)
+        return r2_score(y, y_pred)
